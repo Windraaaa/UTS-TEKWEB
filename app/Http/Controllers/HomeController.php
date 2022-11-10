e847593f6a15ec6d3ae435e1fd396ed5032feee3
Windraaaa <114456803+Windraaaa@users.noreply.github.com>
2022-11-10 19:42:19 +0800

    Add files via upload

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    function index(){
+        return view('frontpage.landingpage', ['title' => "Landing Page"]);
+    }
+
+    function page(){
+        return view('frontpage.page', ['title' => 'page']);
+    }
+    
+}
